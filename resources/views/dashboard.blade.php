d6c549d9d98cd69d9f3e9b14a5b0e6b1b4dbad47
adharaka <150102828+adharaka@users.noreply.github.com>
2025-04-28 21:29:49 +0700

    Update dashboard.blade.php

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <header>Selamat datang</header>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+</head>
+<body>
+    <table>
+        <tbody>
+        <th>Foto</th>
+        <th>Nis</th>
+        <th>Nama</th>
+        <th>Alamat</th>
+        <th>Aksi</th>
+        <tr>$foto</tr>
+        <tr>$nis</tr>
+        <tr>$nama</tr>
+        <th>$alamat</tr>
+        <tr><button>edit</button></tr>
+        <tr><button>delete</button></tr>
+</tbody>
+    </table>
+</body>
+<footer>@Copyright 2025</footer>
+</html>
