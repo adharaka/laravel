a925c136ddf49c2dbaf270403f20a85da2dc42a0
adharaka <150102828+adharaka@users.noreply.github.com>
2025-04-28 22:08:23 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SiswaController;
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-});
-
-Route::get('/tambahsiswa', function () {
-    return view('tambah');
-});
-
-Route::get('/detail', function () {
-    return view('detail');
-});
+Route::get('/', [SiswaController::class, 'index'])->name('siswa.index');
+Route::get('/siswa/{nis}', [SiswaController::class, 'show'])->name('siswa.show');
+Route::get('/create', [SiswaController::class, 'create'])->name('siswa.create');
+Route::post('/store', [SiswaController::class, 'store'])->name('siswa.store');
