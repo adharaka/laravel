a09eb9c2a605f4729973a0457befb417b6b0ba4d
adharaka <150102828+adharaka@users.noreply.github.com>
2025-04-28 21:29:03 +0700

    Update detail.blade.php

---
+++ resources/views/detail.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profil siswa</title>
+</head>
+<body>
+    <h1>Siti aminah</h1>
+    <p>NIS:{{$nis}}</p>
+    <P>Alamat:{{$alamat}}</P>
+    <p>Tanggal lahir:{{$tanggal_lahir}}</p>
+</body>
+</html>
