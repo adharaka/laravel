a26ace01c358b90e42c016e9754ce73a39e94d7c
adharaka <150102828+adharaka@users.noreply.github.com>
2025-04-28 21:30:41 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+});
+
+Route::get('/tambahsiswa', function () {
+    return view('tambah');
+});
+
+Route::get('/detail', function () {
+    return view('detail');
 });
